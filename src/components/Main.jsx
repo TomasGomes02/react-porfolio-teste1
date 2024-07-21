383fc42168b0c554d0e5f10ec7f3ef80bee8f0ea
tomas <a48614@alunos.isel.pt>
2024-07-21 03:27:29 +0100

    v3

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
             <img className='w-full h-screen object-cover object-center' src="https://www.apetogentleman.com/wp-content/uploads/2022/01/Patrick-Bateman-American-Psycho-2048x1195.jpg"/>
             <div className='w-full h-screen absolute top-0 left-0 bg-white/35'>
             <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
-                <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Tomás TESTE</h1>
+                <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Tomás</h1>
                 <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I'm a 
                 <TypeAnimation
                     sequence={[
@@ -23,7 +23,7 @@ const Main = () => {
                     wrapper="div"
                     cursor={true}
                     speed={50}
-                    style={{ fontSize: '1em', paddingLeft: '20px'}}
+                    style={{ fontSize: '1em', paddingLeft: '10px'}}
                     repeat={Infinity}
                     />
                 </h2>
